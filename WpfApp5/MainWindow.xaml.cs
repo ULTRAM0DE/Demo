b21588310a91d2ca79d8fac81ec2017c5aab481e
0NE <0netw08778@gmail.com>
2021-10-02 22:58:45 +0400

    Добавьте файлы проекта.

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp5.DB;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var user = Auntification.AuntificationUser(txLogin.Text, txPassword.Text);
+            MessageBox.Show("Добро пожаловать" + " " + user.Name);
+            View.WindowMenu windowMenu = new View.WindowMenu();
+            windowMenu.Show();
+            this.Close();
+        }
+
+        
+    }
+    public class Auntification
+    {
+        public static DB.User AuntificationUser(string login, string password)
+        {
+            try
+            {
+                DB.TsaplinEntities entities = new DB.TsaplinEntities();
+                var user = entities.User.Single(x => x.Login == login && x.Password == password);
+                return user;
+            }
+            catch
+            {
+                throw new Exception("Ошибка");
+            }
+        }
+    }
+
+}
---
+++ WpfApp5/View/WindowMenu.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp5.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowMenu.xaml
+    /// </summary>
+    public partial class WindowMenu : Window
+    {
+        public WindowMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            View.WindowAllMaterials windowAllMaterials = new WindowAllMaterials();
+            windowAllMaterials.Show();
+            this.Close();
+        }
+    }
+}
